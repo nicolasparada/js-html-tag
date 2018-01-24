fcb21419cc38270858be9ae19e42e0ee73bc6280
Nicolás Parada <parada.nicolas@outlook.com>
2018-01-24 06:57:29 -0300

    Handle arrays and functions

--- html-tag.js
+++ html-tag.js
@@ -1,6 +1,10 @@
 const htmlValue = value => value instanceof Element
     ? value.innerHTML
-    : String(value)
+    : Array.isArray(value)
+        ? value.join('')
+        : typeof value === 'function'
+            ? `javascript: (${String(value)})(event)`
+            : String(value)
 
 /**
  * @param {TemplateStringsArray} strings
@@ -9,8 +13,9 @@ const htmlValue = value => value instanceof Element
 export default function html(strings, ...values) {
     const rawStrings = strings.raw
     const template = document.createElement('template')
-    template.innerHTML = values.reduce((acc, v, i) =>
-        acc + htmlValue(v) + rawStrings[i + 1], rawStrings[0])
+    template.innerHTML = values
+        .reduce((acc, v, i) => acc + htmlValue(v) + rawStrings[i + 1], rawStrings[0])
+        .replace(/^\s+|\s+$/g, '')
     return template.content.childNodes.length === 1
         ? /** @type {HTMLElement} */ (template.content.firstChild)
         : template.content
