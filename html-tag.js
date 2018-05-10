691512b0e22104d4233a9c2e8e7077f8ea090308
Nicolás Parada <parada.nicolas@outlook.com>
2018-05-09 22:12:05 -0300

    htmlLiteral

--- html-tag.js
+++ html-tag.js
@@ -1,23 +1,48 @@
-const isObject = x => typeof x === 'object' && x !== null
-
-const htmlValue = value => value instanceof Element
-    ? value.innerHTML
-    : Array.isArray(value)
-        ? value.map(htmlValue).join('')
-        : isObject(value)
-            ? JSON.stringify(value)
-            : String(value)
-
+/**
+ * @param {TemplateStringsArray} strings
+ * @param {...any} values
+ */
+export function html(strings, ...values) {
+    const template = document.createElement('template')
+    template.innerHTML = makeHTML(strings, values)
+    return template
+}
 
 /**
  * @param {TemplateStringsArray} strings
  * @param {...any} values
  */
-export default function html(strings, ...values) {
-    const rawStrings = strings.raw
-    const template = document.createElement('template')
-    template.innerHTML = values
-        .reduce((acc, v, i) => acc + htmlValue(v) + rawStrings[i + 1], rawStrings[0])
-        .replace(/^\s+|\s+$/g, '')
-    return template
+export function htmlLiteral(strings, ...values) {
+    return new LiteralString(makeHTML(strings, values))
+}
+
+class LiteralString {
+    constructor(string) {
+        this.value = string.toString()
+    }
+
+    toString() {
+        return this.value
+    }
+}
+
+function makeHTML(strings, values) {
+    return values
+        .reduce((acc, v, i) => acc + stringify(v) + strings[i + 1], strings[0])
+        .replace(/^\s+|\s+$/gu, '')
+}
+
+/**
+ * @returns {string}
+ */
+function stringify(x) {
+    return x instanceof HTMLTemplateElement
+        ? x.innerHTML
+        : x instanceof LiteralString
+            ? x.value
+            : Array.isArray(x)
+                ? x.map(stringify).join('\n')
+                : typeof x === 'object' && x !== null
+                    ? JSON.stringify(x, null, 2)
+                    : String(x)
 }
