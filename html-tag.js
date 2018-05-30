ff217374c978174d76c415d4f0e23c64fcba0ddc
Nicolás Parada <parada.nicolas@outlook.com>
2018-05-30 15:39:23 -0400

    Reduce function scope

--- html-tag.js
+++ html-tag.js
@@ -1,48 +1,21 @@
 /**
+ * Tag to create an HTMLTemplateElement.
+ *
  * @param {TemplateStringsArray} strings
- * @param {...any} values
+ * @param {any[]} values
+ * @returns {HTMLTemplateElement}
  */
-export function html(strings, ...values) {
+export default function html(strings, ...values) {
     const template = document.createElement('template')
-    template.innerHTML = makeHTML(strings, values)
+    template.innerHTML = values
+        .reduce((acc, v, i) => acc + htmlValue(v) + strings[i + 1], strings[0])
     return template
 }
 
-/**
- * @param {TemplateStringsArray} strings
- * @param {...any} values
- */
-export function htmlLiteral(strings, ...values) {
-    return new LiteralString(makeHTML(strings, values))
-}
-
-class LiteralString {
-    constructor(string) {
-        this.value = string.toString()
-    }
-
-    toString() {
-        return this.value
-    }
-}
-
-function makeHTML(strings, values) {
-    return values
-        .reduce((acc, v, i) => acc + stringify(v) + strings[i + 1], strings[0])
-        .replace(/^\s+|\s+$/gu, '')
-}
-
-/**
- * @returns {string}
- */
-function stringify(x) {
-    return x instanceof HTMLTemplateElement
-        ? x.innerHTML
-        : x instanceof LiteralString
-            ? x.value
-            : Array.isArray(x)
-                ? x.map(stringify).join('\n')
-                : typeof x === 'object' && x !== null
-                    ? JSON.stringify(x, null, 2)
-                    : String(x)
+function htmlValue(value) {
+    return value instanceof HTMLTemplateElement
+        ? value.innerHTML
+        : Array.isArray(value)
+            ? value.map(htmlValue).join('\n')
+            : String(value)
 }
